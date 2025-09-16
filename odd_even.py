73df26ffe65dc4bb50709f19005702d5f568b5d1
Shane Wagar <80860993+sunbrolynk@users.noreply.github.com>
2025-09-15 18:35:07 -0600

    Delete odd_even.py
    remove unnecessary components

--- odd_even.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to Is it Odd or Even!")
-favorite_number = int(input("What is your favorite number?\n"))
-#converted_number = int(favorite_number)
-odd_even = favorite_number % 2
-
-if odd_even == 0:
-    print("Its even!")
-else:
-    print("It's Odd!")
